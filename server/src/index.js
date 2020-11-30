c8999033ac629074eafd073c6252e0df4a6dca75
Agney <agney.menon@bigbinary.com>
2020-12-01 01:50:24 +0530

    setup cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+var cors = require('cors');
 const { postgraphile } = require("postgraphile");
 
 const app = express();
+
+app.use(cors());
 
 app.use(
   postgraphile(
